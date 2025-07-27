bdbc7135045439c4e3834575addd5083794d6957
Bhavya Dalal <dalalbhavya@gmail.com>
2025-07-28 00:44:20 +0530

    added rviz visualization

--- src/dwa_planner_bd/launch/dwa_planner_launch.py
+++ src/dwa_planner_bd/launch/dwa_planner_launch.py
@@ -16,8 +16,11 @@ def generate_launch_description():
     # Package share directories.
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
     pkg_turtlebot3_gazebo = get_package_share_directory('turtlebot3_gazebo')
+    pkg_dwa_planner_bd = get_package_share_directory('dwa_planner_bd')
 
-    # Define the world file path.
+
+    # paths
+    rviz_config_path = os.path.join(pkg_dwa_planner_bd, 'rviz', 'dwa.rviz')
     world_file_path = os.path.join(pkg_turtlebot3_gazebo, 'worlds', 'turtlebot3_world.world')
 
     set_turtlebot_model_env_var = SetEnvironmentVariable(
@@ -51,6 +54,15 @@ def generate_launch_description():
         }.items()
     )
 
+    # RViz configuration
+    launch_rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_path],
+        output='screen'
+    )
+
     # spawn turtlebot3 in gazebo
     spawn_turtlebot_node = Node(
         package='gazebo_ros',
@@ -58,8 +70,8 @@ def generate_launch_description():
         arguments=[
             '-entity', 'turtlebot3_burger',
             '-file', os.path.join(pkg_turtlebot3_gazebo, 'models', 'turtlebot3_burger', 'model.sdf'),
-            '-x', '0.0',
-            '-y', '0.0',
+            '-x', '0.5',
+            '-y', '0.5',
             '-z', '0.3'
         ],
         output='screen'
@@ -69,6 +81,7 @@ def generate_launch_description():
     ld.add_action(launch_gazebo)
     ld.add_action(launch_robot_state_publisher)
     ld.add_action(spawn_turtlebot_node)
+    ld.add_action(launch_rviz)
     ld.add_action(node_dwa)
 
     return ld
